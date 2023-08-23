d28d986340a1abeb723ebc3e1c2d668b5c72ad31
recursive-beast <splentercell.1997@gmail.com>
2023-08-23 14:36:15 +0100

    ci: configure pre-release build

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["master"],
+  branches: ["master", { name: "pre-release", prerelease: true }],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
